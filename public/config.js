f483a7eb3facb8c46b9f8809ae2870c34ef54520
Daham <dahampositha33@gmail.com>
2024-04-14 19:58:05 +0530

    Delete public/config.js

--- public/config.js
+++
@@ -1,9 +0,0 @@
-window.configs = {
-    orderApiUrl: "http://localhost:8080",
-    auth: {
-        signInRedirectURL: "http://localhost:3000",
-        signOutRedirectURL: "http://localhost:3000",
-        clientID: "",
-        baseUrl: "https://api.asgardeo.io/t/hyperluminous",
-    }
-}
